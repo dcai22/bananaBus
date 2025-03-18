2d0bc8481102cdf9e7041af74f441b63864efe77
Anson <z5416611@ad.unsw.edu.au>
2025-03-18 17:59:41 +1100

    prettyfied login page

--- banana-bus/app/(tabs)/login.tsx
+++ banana-bus/app/(tabs)/login.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 import { Link, useNavigation } from "expo-router";
 
@@ -15,26 +15,32 @@ export default function LoginScreen() {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>BIG BANANA</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Email"
-                value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
-            <TextInput
-                style={[styles.input, styles.password]}
-                placeholder="Password"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
-            <Link href="/(tabs)/register">
-                <Button title="Register" />
-            </Link>
-            <Button title="Login" onPress={handleLogin} />
+            <View style={styles.title}>
+                <Text style={styles.title}>banana bus      🚌</Text>
+            </View>
+            <View style={styles.form}> 
+                <TextInput
+                    style={styles.input}
+                    placeholder="email"
+                    value={email}
+                    onChangeText={setEmail}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={[styles.input, styles.password]}
+                    placeholder="password"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
+                <TouchableOpacity onPress={handleLogin} style={[styles.button, styles.loginButton]}>
+                    <Text style={[styles.buttonText, styles.loginText]}>Login →</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate('register')} style={[styles.button, styles.registerButton]}>
+                    <Text style={[styles.buttonText, styles.registerText]}>Register</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -44,21 +50,60 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        padding: 16,
+        padding: 40,
+        backgroundColor: '#7CBBF1',
     },
     title: {
-        fontSize: 24,
-        marginBottom: 16,
+        fontSize: 56,
+        fontWeight: 'bold',
+        color: '#fff',
+        paddingHorizontal: 20,
+        marginBottom: 15,
+    },
+    icon: {
+        width: 50,
+        height: 50,
+        marginBottom: 20,
+    },
+    form: {
+        width: '80%',
+        alignItems: 'center',
     },
     input: {
         width: '100%',
-        padding: 8,
-        margin: 2,
+        padding: 10,
+        margin: 10,
         borderWidth: 1,
         borderColor: '#ccc',
-        borderRadius: 4,
+        borderRadius: 8,
+        backgroundColor: '#fff',
     },
-    password: {
-        marginBottom: 16,
+    button: {
+        width: '100%',
+        padding: 15,
+        marginVertical: 10,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    loginButton: {
+        backgroundColor: '#ccff00',
+    },
+    registerButton: {
+        backgroundColor: '#2A8AE4',
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+    loginText: {
+        color: '#2A8AE4',
+    },
+    registerText: {
+        color: '#fff',
+    },
+    forgotPassword: {
+        color: '#007bff',
+        marginTop: 10,
     },
 });
