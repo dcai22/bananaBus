1da3022764838d63d5802d3870e1b0511edcdfe6
Austin - 2023 technical <austinhoe@protonmail.com>
2025-04-17 23:50:54 +1000

    added editVehicle function

--- banana-bus/api/manager.ts
+++ banana-bus/api/manager.ts
@@ -1,5 +1,5 @@
 import HTTPError from "http-errors";
-import { ObjectId  } from "mongodb";
+import { ObjectId, DeleteResult  } from "mongodb";
 import { collections, connectToDatabase } from "./mongoUtil";
 import { User, Vehicle } from "./interface";
 import { findUserByToken } from "./helper";
@@ -46,10 +46,8 @@ export async function addVehicle(maxCapacity: number, maxLuggageCapacity: number
     
     const checkVehicleExist = await collections.vehicles?.findOne({ numberPlate: numberPlate });
         if (checkVehicleExist) {
-            throw HTTPError(400, 'Vehicle with number plate already in database');
-        }
-
-    
+            throw HTTPError(409, 'Vehicle with number plate already in database');
+        }    
 
     const newVehicle: Vehicle = {
         _id: new ObjectId(),
@@ -69,7 +67,7 @@ export async function addVehicle(maxCapacity: number, maxLuggageCapacity: number
 }
 
 
-
+// TODO: deleted vehicles should also remove all vehicle IDs from trips
 export async function deleteVehicle(vehicleId: ObjectId) {
     await connectToDatabase();
 
@@ -77,21 +75,47 @@ export async function deleteVehicle(vehicleId: ObjectId) {
         throw HTTPError(500, 'Database collection is not initialized');
     }
 
-    const vehicle = await collections.vehicles?.findOne({ _id: vehicleId });
-    if (!vehicle) {
-        throw HTTPError(400, 'vehicle not found');
+    const result: DeleteResult = await collections.vehicles.deleteOne({ _id: vehicleId });
+    
+    if (result.deletedCount === 0) {
+        throw HTTPError(404, 'Vehicle not found');
     }
         
-    user.isManager = false;
-    await collections.users?.updateOne({ userId: user._id }, user);
-    return user;
-
-    
+    return {};
 }
 
-export async function editVehicle(){
+export async function editVehicle( vehicleId: ObjectId, maxCapacity: number, maxLuggageCapacity: number, hasAssist: boolean,numberPlate: string): Promise<Vehicle>{
     await connectToDatabase();
 
+    if (!collections.vehicles) {
+        throw HTTPError(500, 'Database collection is not initialized');
+    }
+
+    const existing = await collections.vehicles.findOne({ numberPlate, _id: { $ne: vehicleId }});
+    if (existing) {
+        throw HTTPError(409, 'Another vehicle with that number plate already exists');
+    }
+
+    const vehicle = await collections.vehicles.findOne({ _id: vehicleId });
+    if (!vehicle) {
+        throw HTTPError(404, 'Vehicle not found');
+    }
+
+    const result = await collections.vehicles.findOneAndUpdate(
+        { _id: vehicleId },
+        {
+          $set: { maxCapacity, maxLuggageCapacity, hasAssist, numberPlate }
+        },
+        { returnDocument: 'after' }
+      );
+    
+    if (!result) {
+        throw HTTPError(500, 'Failed to update vehicle');
+    }
+
+    return result;
 }
+
+
 
 
