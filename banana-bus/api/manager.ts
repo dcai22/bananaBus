d5d2b76f010f8c65bca808c54a38755e18d4edc6
Austin - 2023 technical <austinhoe@protonmail.com>
2025-04-17 23:28:46 +1000

    reverted change to mongoUtils and fixed add Vehicle

--- banana-bus/api/manager.ts
+++ banana-bus/api/manager.ts
@@ -1,7 +1,7 @@
 import HTTPError from "http-errors";
-import { ObjectId } from "mongodb";
+import { ObjectId  } from "mongodb";
 import { collections, connectToDatabase } from "./mongoUtil";
-import { User } from "./interface";
+import { User, Vehicle } from "./interface";
 import { findUserByToken } from "./helper";
 
 export async function addManager(token: string) {
@@ -35,3 +35,63 @@ export async function removeManager(token: string) {
     await collections.users?.updateOne({ userId: user._id }, user);
     return user;
 }
+
+
+export async function addVehicle(maxCapacity: number, maxLuggageCapacity: number, hasAssist: boolean, numberPlate: string): Promise<Vehicle> {
+    await connectToDatabase();
+
+    if (!collections.vehicles) {
+            throw HTTPError(500, 'Database collection is not initialized');
+        }
+    
+    const checkVehicleExist = await collections.vehicles?.findOne({ numberPlate: numberPlate });
+        if (checkVehicleExist) {
+            throw HTTPError(400, 'Vehicle with number plate already in database');
+        }
+
+    
+
+    const newVehicle: Vehicle = {
+        _id: new ObjectId(),
+        maxCapacity: maxCapacity,
+        maxLuggageCapacity: maxLuggageCapacity,
+        hasAssist: hasAssist,
+        numberPlate: numberPlate
+    };
+
+    const added_vehicle = await collections.vehicles.insertOne(newVehicle);
+    
+    if (!added_vehicle) {
+        throw HTTPError(500, 'Failed to add vehicle to database');
+    }
+
+    return newVehicle;
+}
+
+
+
+export async function deleteVehicle(vehicleId: ObjectId) {
+    await connectToDatabase();
+
+    if (!collections.vehicles) {
+        throw HTTPError(500, 'Database collection is not initialized');
+    }
+
+    const vehicle = await collections.vehicles?.findOne({ _id: vehicleId });
+    if (!vehicle) {
+        throw HTTPError(400, 'vehicle not found');
+    }
+        
+    user.isManager = false;
+    await collections.users?.updateOne({ userId: user._id }, user);
+    return user;
+
+    
+}
+
+export async function editVehicle(){
+    await connectToDatabase();
+
+}
+
+
